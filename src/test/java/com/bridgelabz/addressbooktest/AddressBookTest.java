e78673717afebe98909fada35565ea9285120a2c
Samiksha <shende.samiksha@rediffmail.com>
2021-04-10 19:57:36 +0530

    [Samiksha].ADD code for junit test to perform assertion test for count entries in the database

--- src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
@@ -1,2 +1,20 @@
-package com.bridgelabz.addressbooktest;public class AddressBookTest {
+package com.bridgelabz.addressbooktest;
+
+import com.bridgelabz.addressbook.AddressBook;
+import com.bridgelabz.addressbook.AddressBookData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class AddressBookTest {
+    AddressBook addressBook;
+    List<AddressBookData> addressBookDataList;
+
+    @Test
+    public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        addressBook = new AddressBook();
+        addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
+        Assertions.assertEquals(3, addressBookDataList.size());
+    }
 }
