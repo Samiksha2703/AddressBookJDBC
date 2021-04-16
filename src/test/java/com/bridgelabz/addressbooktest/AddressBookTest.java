1a9c54a1d524e991eb71f31b232ffc938493aa27
Samiksha <shende.samiksha@rediffmail.com>
2021-04-16 07:42:37 +0530

    [Samiksha].Add code to add list of employee using thread indo db

--- src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbooktest/AddressBookTest.java
@@ -5,6 +5,10 @@ import com.bridgelabz.addressbook.AddressBookData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 public class AddressBookTest {
@@ -15,7 +19,7 @@ public class AddressBookTest {
     public void givenThreeContactsInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
         addressBook = new AddressBook();
         addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
-        Assertions.assertEquals(3, addressBookDataList.size());
+        Assertions.assertEquals(7, addressBookDataList.size());
     }
 
     @Test
@@ -45,8 +49,32 @@ public class AddressBookTest {
     public void givenNewContactData_WhenAdded_ShouldSyncWithDB() {
         addressBook = new AddressBook();
         addressBookDataList = addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
-        addressBook.addContactToAddressBook("Samiksha", "Shende", "Ram Nagar", "Wardha", "MH", 442001, "6866577899", "shende.samiksha@gmail.com");
+        addressBook.addContactToAddressBook("Samiksha", "Shende", "Ram Nagar", "Wardha", "MH", 442001,
+                "6866577899", "shende.samiksha@gmail.com");
         boolean result = addressBook.checkAddressBookInSyncWithDB("Samiksha");
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void given6Employees_WhenAdded_Should_ShouldMatchEmpEntries() {
+        AddressBookData[] arrayOfContacts = {
+                new AddressBookData(0, "Neha", "Zade", "Sai Nagar","Nagpur", "MH", 789654,
+                        753964125, "nehazade@gmail.com"),
+        new AddressBookData(0, "Apurva", "Yende", "Keshav Nagar","Pune", "MH", 969654,
+                972431556, "apurva.yende@gmail.com"),
+        new AddressBookData(0, "Sakshi", "Shende", "Reddy Streat","Shriperumbudur", "TN", 549678,
+                925461375, "sakshishende@gmail.com")
+        };
+        AddressBook addressBook = new AddressBook();
+        addressBook.readAddressBookData(AddressBook.IOService.DB_IO);
+        Instant start = Instant.now();
+        addressBook.addContactIntoDB(Arrays.asList(arrayOfContacts));
+        Instant end = Instant.now();
+        System.out.println("Duration without Thread: " + Duration.between(start, end));
+        Instant threadStart = Instant.now();
+        addressBook.addAddressBookDataWithThread(Arrays.asList(arrayOfContacts));
+        Instant threadEnd = Instant.now();
+        System.out.println("Duration with thread: " + Duration.between(threadStart, threadEnd));
+        Assertions.assertEquals(13, addressBook.countEntries(AddressBook.IOService.DB_IO));
     }
 }
